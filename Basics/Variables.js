401b8b580fe932b56f4293b986b8a6bd422ff000
Nikunj Singhania <88815563+NikunjSinghania@users.noreply.github.com>
2023-08-28 06:40:57 +0000

    Commit

---
+++ Basics/Variables.js
@@ -0,0 +1,15 @@
+const nameOfCompany = "RQS Pvt. Ltd."
+let companyEmail = "rsq@india.in"
+var companyPassword = "12345"
+companyCity = "Jaipur";
+let state;
+//Constcannoit be changed
+// {
+//     let a = 12
+// }
+
+// console.log(a);
+
+console.log(nameOfCompany);
+
+console.table([state, companyEmail, companyPassword, companyCity])
