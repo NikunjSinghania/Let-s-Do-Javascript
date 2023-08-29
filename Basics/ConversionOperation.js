5a7bff70d6638df5e33d2f770240fa5196816ffa
Nikunj Singhania <88815563+NikunjSinghania@users.noreply.github.com>
2023-08-29 01:49:51 +0000

    Commit

---
+++ Basics/ConversionOperation.js
@@ -0,0 +1,15 @@
+let score = undefined
+// null 0
+// udefined NAN
+// true 1
+// false 0
+let con = Number(score)
+console.log(typeof con);
+console.log(con);
+
+let boleeanVal = Boolean(score)
+console.log(boleeanVal);
+
+let stringVal = String(score)
+console.log(typeof stringVal);
+console.log(stringVal);
