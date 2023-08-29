eb39d5a6365e79fa3def4fe6b0dae5747f931b23
Nikunj Singhania <88815563+NikunjSinghania@users.noreply.github.com>
2023-08-29 01:29:45 +0000

    Commit

---
+++ Basics/DataType.js
@@ -0,0 +1,14 @@
+"use strict"; // USE NEWER VERSION
+
+console.log(typeof null);
+
+console.log(typeof 1221);
+
+console.log(typeof "String");
+
+console.log(typeof {});
+
+console.log(typeof []);
+
+console.log(typeof true);
+console.log(typeof undefined);
