a4f3a7a1b6a17dbc008571264e84992efb3f34a4
Nikunj Singhania <88815563+NikunjSinghania@users.noreply.github.com>
2023-09-01 09:57:24 +0000

    Commit

---
+++ Basics/Comparison.js
@@ -0,0 +1,18 @@
+// console.log(1 < 32)
+// console.log(2 > 3)
+// console.log(1 == 1)
+// console.log(1 != 4)
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(parseInt(null));
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+
+console.log("2" === 0);
---
+++ Basics/Summary.js
@@ -0,0 +1,23 @@
+//PRIMITIVE TYPE - By Value
+
+// number, string, boolean, null, undefined, Symbol, BigInt
+
+// console.log(typeof 41414124124124n);
+// console.log(typeof true);
+// console.log(typeof "Hello");
+// console.log(typeof null);
+// console.log(typeof undefined);
+// console.log(typeof Symbol(123));
+
+// Refernec - Non Primitive
+
+// array, object, function
+
+console.log(typeof [1,2,3]);
+console.log(typeof { name : 'Nikunj'});
+
+const foo = () =>{
+    console.log('Hello World');
+}
+
+console.log(typeof foo);
