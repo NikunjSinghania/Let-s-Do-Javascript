bcf8ceffce711f3cb10e925474b2c5358d070fac
Nikunj Singhania <88815563+NikunjSinghania@users.noreply.github.com>
2023-09-01 11:42:03 +0000

    Commit

--- Basics/Strings.js
+++ Basics/Strings.js
@@ -7,4 +7,14 @@ console.log(s);
 
 let o = new String('Check')
 
-console.log(o)
+console.log(o.indexOf('c'))
+console.log(o.charAt(0))
+console.log(o.slice(-3, 4));
+
+console.log("   Hello".trimEnd());
+
+console.log("Heel".replace("ee", "eeee"));
+
+console.log("HEL".includes("EL"));
+
+console.log("WORLD".split(""));
