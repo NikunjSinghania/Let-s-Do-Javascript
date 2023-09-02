072095718220eb5bf51e451e735387e1d183c07f
Nikunj Singhania <88815563+NikunjSinghania@users.noreply.github.com>
2023-09-02 04:29:52 +0000

    Array

---
+++ Basics_II/Array.js
@@ -0,0 +1,40 @@
+let arr = [1,2,3]
+
+// console.log(new Array(1,2,3));
+
+arr[0] = 10;
+
+// console.log(arr);
+
+let arr_2 = arr;
+
+// console.log(arr_2);
+
+arr[2] = 20
+
+// console.log(arr_2);
+
+arr.push(1231);
+arr.pop()
+
+arr.unshift(12414)
+arr.shift()
+
+// console.log(arr.includes(20));
+// console.log(arr.indexOf(1));
+// console.table(arr);
+// console.log(arr.indexOf(10));
+
+let arr_3 = arr.join()
+
+
+// console.log(arr_3);
+// console.log(arr.join());
+// console.log(typeof arr.join());
+
+console.log(arr);
+// console.log(arr.slice(1,2));
+// console.log(arr.slice(-3, -1));
+
+console.log(arr.splice(1, 3));
+console.log(arr);
